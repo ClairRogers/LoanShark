65073965215898291f05c5eb814f06af46c20095
Clair Rogers <clair-rogers@outlook.com>
2019-04-05 20:06:03 -0600

    heroku AGAIN

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -2,31 +2,32 @@
   <div class="login container-fluid login-forms">
     <div class="row">
       <div class="col-12">
-        <h1>LoanShark</h1>
       </div>
     </div>
 
     <div class="row">
       <div class="form-container col-12 d-flex align-items-end justify-content-center">
-        <form v-if="loginForm" @submit.prevent="loginUser">
-          <input type="email" v-model="creds.email" placeholder="email" class="mr-4 login-form">
-          <input type="password" v-model="creds.password" placeholder="password" class="login-form">
-          <br>
-          <button type="submit" class="btn btn-outline-success mt-3 text-center">Login</button>
-        </form>
-        <form v-else @submit.prevent="register" class="mt-2 text-center">
-          <input type="text" v-model="newUser.name" placeholder="name" class="login-form mr-4">
-          <input type="email" v-model="newUser.email" placeholder="email" class="login-form">
-          <input type="password" v-model="newUser.password" placeholder="password" class="login-form ml-4">
-          <br>
-          <button type="submit" class="btn btn-outline-success mt-3 text-center">Create Account</button>
-        </form>
+        <div>
+          <h4 class="mb-4">Welcome to LoanShark!</h4>
+          <form v-if="loginForm" @submit.prevent="loginUser">
+            <input type="email" v-model="creds.email" placeholder="email" class="mx-2 login-form">
+            <input type="password" v-model="creds.password" placeholder="password" class="mx-2 login-form">
+            <br>
+            <button type="submit" class="btn btn-outline-success mt-3 text-center">Login</button>
+          </form>
+          <form v-else @submit.prevent="register" class="mt-2 text-center">
+            <input type="text" v-model="newUser.name" placeholder="name" class="login-form mx-2">
+            <input type="email" v-model="newUser.email" placeholder="email" class="login-form mx-2">
+            <input type="password" v-model="newUser.password" placeholder="password" class="login-form mx-2">
+            <br>
+            <button type="submit" class="btn btn-outline-success mt-3 text-center">Create Account</button>
+          </form>
+        </div>
       </div>
     </div>
     <div class="row">
       <div class="col-12 form-container col-12 d-flex align-items-start justify-content-center"">
-        <div class="
-        action mt-3" @click="loginForm = !loginForm">
+        <div class=" action mt-3" @click="loginForm = !loginForm">
         <p v-if="loginForm">No account? Click here to Register</p>
         <p v-else>Already have an account? Click here to Login</p>
 
@@ -75,13 +76,14 @@
 
   input {
     height: 50px;
-    width: 400px;
+    min-width: 300px;
+    max-width: 400px;
     text-align: center;
     border: 1px solid rgba(224, 224, 224, 0.473);
   }
 
   .form-container {
-    height: 50vh
+    height: 40vh
   }
 
   .note-container {
