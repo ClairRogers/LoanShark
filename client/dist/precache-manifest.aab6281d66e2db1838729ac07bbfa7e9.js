19adbcf09138ff28a44f353352f95c53e163e4ef
Clair Rogers <clair-rogers@outlook.com>
2019-04-05 18:06:43 -0600

    new build

--- client/dist/precache-manifest.aab6281d66e2db1838729ac07bbfa7e9.js
+++ client/dist/precache-manifest.aab6281d66e2db1838729ac07bbfa7e9.js
@@ -4,12 +4,12 @@ self.__precacheManifest = [
     "url": "/js/about.d288b4f1.js"
   },
   {
-    "revision": "2a9f6d7449e061095b27",
-    "url": "/css/app.c2103b4a.css"
+    "revision": "d92e66670b968f0bae2c",
+    "url": "/css/app.33082f4e.css"
   },
   {
-    "revision": "2a9f6d7449e061095b27",
-    "url": "/js/app.43c274d5.js"
+    "revision": "d92e66670b968f0bae2c",
+    "url": "/js/app.0c55be70.js"
   },
   {
     "revision": "d4b051b68a4d2218be0e",
@@ -20,16 +20,8 @@ self.__precacheManifest = [
     "url": "/img/LoanShark-white.405e963d.png"
   },
   {
-    "revision": "1d8c4e85aeea9dfc4d8447ee7979c215",
+    "revision": "d81b26c1c35473f93651796326263553",
     "url": "/index.html"
-  },
-  {
-    "revision": "ce03b0ae3667ad3333982db457f949c5",
-    "url": "/LoanShark-16x16.png"
-  },
-  {
-    "revision": "3fbd35d27eee4bd65ef81113f946ad39",
-    "url": "/LoanShark-32x32.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
